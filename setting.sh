9d86f138f118c2e34f0ed50835c652c9953f31b5
LeeHyeonKyu <hklee128@naver.com>
2022-06-08 06:04:30 +0000

    Update

--- setting.sh
+++ setting.sh
@@ -1,4 +1,7 @@
 pip install wandb
+
 cd /content/2022_ai_competition
+cp -r /content/drive/MyDrive/2022_ai_competition/dataset /content/2022_ai_competition/.
+
 git config --global user.email hklee128@naver.com
 git config --global user.name LeeHyeonKyu
